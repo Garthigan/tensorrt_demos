5a829a10f99ea90dc18432d27af8abbed3cb8dbc
dimuthsenzmate <dimuth@senzmate.com>
2023-11-20 14:56:05 +0530

    done2

--- mask/mask.py
+++ mask/mask.py
@@ -3,18 +3,13 @@ import matplotlib.pyplot as plt
 import cv2
 import json
 
-variable_nano_file_path = "/home/jetson-nl1/Downloads/tensorrt_demos/variable_nano.json"
-with open(variable_nano_file_path, 'r') as file:
-    json_data = file.read()
-parsed_data = json.loads(json_data)
-mask_coordinates = parsed_data['mask_coordinates']
 
 
-def plot_line(frame):
 
-    img_name = "points/sample.png"
-    img = cv2.imread(img_name)
-    h, w = img.shape[:2]
+def plot_line(frame  , mask_coordinates, sample_image):
+
+
+    h, w = sample_image
     height, width = frame.shape[:2]
 
     edge_coordinates = [[int((a / w) * width), int((b / h) * height)] for a, b in mask_coordinates]
--- object_tracker.py
+++ object_tracker.py
@@ -1,35 +1,20 @@
 # nolimit-kandy -format
-
 import os
 import time
-import crop
-
-# comment out below line to enable tensorflow logging outputs
-print("initializing")
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
 import cv2
-from mask import mask as md
 import psutil
 import pycuda.autoinit
 import tensorflow as tf
 import json
-
-# change number of blocks per thread
-physical_devices = tf.config.experimental.list_physical_devices("GPU")
-if len(physical_devices) > 0:
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-print(physical_devices)
-
 import queue
 import threading
-
-import core.utils as utils
+from PIL import Image
+from tensorflow.compat.v1 import ConfigProto, InteractiveSession
+from tensorflow.python.saved_model import tag_constants
 import matplotlib.pyplot as plt
 import numpy as np
 import paho.mqtt.client as mqtt
+import core.utils as utils
 from absl import app, flags, logging
 from absl.flags import FLAGS
 from core.config import cfg
@@ -37,11 +22,25 @@ from core.yolov4 import filter_boxes
 from deep_sort import nn_matching, preprocessing
 from deep_sort.detection import Detection
 from deep_sort.tracker import Tracker
-from PIL import Image
-from tensorflow.compat.v1 import ConfigProto, InteractiveSession
-from tensorflow.python.saved_model import tag_constants
 from tools import generate_detections as gdet
 from utils.yolo_with_plugins import TrtYOLO
+from mask import mask as md
+
+
+
+# comment out below line to enable tensorflow logging outputs
+print("initializing")
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+
+# change number of blocks per thread
+physical_devices = tf.config.experimental.list_physical_devices("GPU")
+if len(physical_devices) > 0:
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+print(physical_devices)
+
 
 
 variable_nano_file_path = "/home/jetson-nl1/Downloads/tensorrt_demos/variable_nano.json"
@@ -49,6 +48,9 @@ with open(variable_nano_file_path, 'r') as file:
     json_data = file.read()
 parsed_data = json.loads(json_data)
 rtsp_link = parsed_data['rtsp_link']
+mask_coordinates = parsed_data['mask_coordinates']
+sample_image = parsed_data['sample_image']
+
 
 flags.DEFINE_integer("category_num", 80, "number of object categories [80]")
 flags.DEFINE_string("output", None, "path to output video")
@@ -147,7 +149,7 @@ def main(_argv):
     count = 0
     frame = vid.read()
 
-    mask_2 = md.plot_line(frame)
+    mask_2 = md.plot_line(frame  , mask_coordinates, sample_image)
     print(mask_2)
 
     # Create the log file
